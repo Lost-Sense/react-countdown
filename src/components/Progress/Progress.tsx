93a7ef767b08f98f15d083a43689260f66a8a052
Vlad Klepikov <klepikov.prodaction@gmail.com>
2024-10-25 21:50:09 +0300

    fix

--- src/components/Progress/Progress.tsx
+++ src/components/Progress/Progress.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import s from './Progress.module.css';
 import { TypeProgress } from './Progress.type';
 
@@ -11,6 +11,8 @@ export function Progress({
   setMinutes,
   setSeconds,
 }: TypeProgress) {
+  const [totalValue, setTotalValue] = useState(1);
+
   useEffect(() => {
     const totalSeconds = minutes * 60 + seconds;
     setValue(totalSeconds);
@@ -21,18 +23,34 @@ export function Progress({
     setSeconds(value % 60);
   }, [value]);
 
+  useEffect(() => {
+    if (!start) {
+      const totalValues = value;
+      setTotalValue(totalValues);
+    }
+    if (value === 0) {
+      const totalValues = 0;
+      setTotalValue(totalValues);
+    }
+  }, [value, start]);
+
   const rangeChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setValue(Number(event.target.value));
   };
   return (
-    <input
-      className={s.input}
-      onChange={rangeChange}
-      type="range"
-      value={value}
-      max="3599"
-      step="15"
-      disabled={start ? true : false}
-    ></input>
+    <>
+      <input
+        className={s.input}
+        onChange={rangeChange}
+        type="range"
+        value={value}
+        max="3599"
+        step="15"
+        disabled={start ? true : false}
+      ></input>
+      <div className={s.percent}>
+        {value !== 0 ? ((value / totalValue) * 100).toFixed(2) + ' %' : 'Done'}
+      </div>
+    </>
   );
 }
